9a67014e858648a5c9ba04d18b198b0ee822a776
hwnim5324 <hwnim5324@gmail.com>
2025-01-13 17:00:43 +0900

    [feat] html 파일 실행이 아닌 대부분의 web server runner에서 절대 경로 인식 불가로 롤백 - vite.config, package.json 롤백

--- vite.config.ts
+++ vite.config.ts
@@ -3,29 +3,16 @@ import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  if (mode === 'dev') {
-    return {
-      plugins: [
-        react({
-          jsxImportSource: '@emotion/react',
-          tsDecorators: true,
-        }),
-        tsconfigPaths(),
-      ],
-      server: {
-        port: 5100,
-      },
-    };
-  }
-  return {
-    base: './',
-    plugins: [
-      react({
-        jsxImportSource: '@emotion/react',
-        tsDecorators: true,
-      }),
-      tsconfigPaths(),
-    ],
-  };
+export default defineConfig({
+  base: './',
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+      tsDecorators: true,
+    }),
+    tsconfigPaths(),
+  ],
+  server: {
+    port: 5100,
+  },
 });
