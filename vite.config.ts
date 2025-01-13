44a20632aff9ddf939c68ef7e98c2f054ddeec88
hwnim5324 <hwnim5324@gmail.com>
2025-01-13 16:49:12 +0900

    [feat] build 실패 오류 수정 - build 시 불필요하게 tsc 명령어가 실행되던 내용 제거 - build 시 상대경로로 변경되도록 vite.config 파일 수정

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,29 @@ import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      jsxImportSource: '@emotion/react',
-      tsDecorators: true,
-    }),
-    tsconfigPaths(),
-  ],
-  server: {
-    port: 5100,
-  },
+export default defineConfig(({ mode }) => {
+  if (mode === 'dev') {
+    return {
+      plugins: [
+        react({
+          jsxImportSource: '@emotion/react',
+          tsDecorators: true,
+        }),
+        tsconfigPaths(),
+      ],
+      server: {
+        port: 5100,
+      },
+    };
+  }
+  return {
+    base: './',
+    plugins: [
+      react({
+        jsxImportSource: '@emotion/react',
+        tsDecorators: true,
+      }),
+      tsconfigPaths(),
+    ],
+  };
 });
